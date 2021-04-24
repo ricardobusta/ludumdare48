bb612d726915b408f9ffb68a387a153bac68afb5
Ricardo Bustamante <ricardo@busta.dev>
2021-04-24 18:03:24 +0200

    Implement a generated mesh map that can loop.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,40 +1,95 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace Busta.Diggy
 {
     public class GameController : MonoBehaviour
     {
-        public GameObject rowPrefab;
-        public GameObject dirtPrefab;
-        public GameObject hazardPrefab;
+        public Vector2Int gridSize;
+        public int decoWidth;
 
-        public GameObject gridContainer;
+        public GridMesh gridMesh;
 
-        public Vector2Int boardSize;
+        public int initialOffset;
+        public Vector2Int initialPosition;
+        public int maxYPosition;
 
-        private GameObject[] rows;
+        private int _yPosition;
+        private int _offset;
+
+        private int[][] _grid;
+
+        private Tween _gridTween;
 
         private void Start()
         {
-            rows = new GameObject[boardSize.y];
+            _offset = initialOffset;
+            _yPosition = initialPosition.y;
 
-            for (var j = 0; j < boardSize.y; j++)
+            _grid = new int[gridSize.y][];
+            for (var i = 0; i < gridSize.y; i++)
             {
-                var row = rows[j] = Instantiate(rowPrefab, gridContainer.transform);
+                _grid[i] = new int[gridSize.x];
 
-                var dirtPos = Random.Range(0, 2) % 2 == 0 ? 0 : 2;
+                GenerateRow(i);
+            }
 
-                for (var i = 0; i < boardSize.x; i++)
+            gridMesh.InitDecor(gridSize, decoWidth);
+
+            gridMesh.UpdateMesh(_grid, _offset);
+
+            gridMesh.transform.position = new Vector3(initialPosition.x, initialPosition.y, 0);
+        }
+
+        private void GenerateRow(int i)
+        {
+            var hazardJ = Random.Range(0, 2) * 2;
+
+            for (var j = 0; j < gridSize.x; j++)
+            {
+                if (j == hazardJ)
                 {
-                    Instantiate(dirtPos == i ? hazardPrefab : dirtPrefab, new Vector3(i - 1, -j, 0),
-                        Quaternion.identity,
-                        row.transform);
+                    _grid[i][j] = 2;
+                }
+                else
+                {
+                    _grid[i][j] = 1;
                 }
             }
         }
 
-        void Update()
+        private void Update()
         {
+            var input = Input.GetKeyDown(KeyCode.LeftArrow) ? -1 : Input.GetKeyDown(KeyCode.RightArrow) ? 1 : 0;
+            if (input != 0)
+            {
+                _gridTween?.Kill();
+
+                var row = (_yPosition + 1) % _grid.Length;
+                _grid[row][0] = input == -1 ? 0 : _grid[row][0];
+                _grid[row][1] = 0;
+                _grid[row][2] = input == 1 ? 0 : _grid[row][2];
+
+                _yPosition++;
+
+                var pos = gridMesh.transform.position;
+
+                if (_yPosition > maxYPosition)
+                {
+                    row = _offset % _grid.Length;
+                    GenerateRow(row);
+                    _offset++;
+                    
+                    gridMesh.transform.position = pos + Vector3.down;
+                    _gridTween = gridMesh.transform.DOMoveY(maxYPosition, 0.3f);
+                }
+                else
+                {
+                    _gridTween = gridMesh.transform.DOMoveY(_yPosition, 0.3f);
+                }
+                
+                gridMesh.UpdateMesh(_grid, _offset);
+            }
         }
     }
 }
--- Assets/Scripts/GridMesh.cs
+++ Assets/Scripts/GridMesh.cs
@@ -1,50 +1,158 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 namespace Busta.Diggy
 {
     [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
     public class GridMesh : MonoBehaviour
     {
-        public Vector2Int gridSize;
+        public Vector2[] uvOffsets;
+        public int textureSize;
 
         public Mesh frontFaceTemplate;
+        public Mesh backFaceTemplate;
 
-        private List<Vector3> _verticeBuffer;
+        public Mesh topFaceTemplate;
+        public Mesh bottomFaceTemplate;
+        public Mesh leftFaceTemplate;
+        public Mesh rightFaceTemplate;
+
+        public Mesh topLeftCornerTemplate;
+        public Mesh topRightCornerTemplate;
+        public Mesh bottomLeftCornerTemplate;
+        public Mesh bottomRightCornerTemplate;
+
+        public MeshFilter decorationLeft;
+        public MeshFilter decorationRight;
+
+        private List<Vector3> _vertexBuffer;
         private List<Vector2> _uvBuffer;
         private List<int> _triangleBuffer;
         private List<Vector3> _normalBuffer;
 
+        private Mesh _mesh;
+        private Vector2[] _uvOffsets;
+
         private void Start()
         {
-            var meshFilter = GetComponent<MeshFilter>();
-            var mesh = meshFilter.mesh = new Mesh();
+            _uvOffsets = uvOffsets.Select(uv => uv * (1f / textureSize)).ToArray();
 
-            _verticeBuffer = new List<Vector3>();
+            var meshFilter = GetComponent<MeshFilter>();
+            _mesh = meshFilter.mesh = new Mesh();
+
+            _vertexBuffer = new List<Vector3>();
             _triangleBuffer = new List<int>();
             _uvBuffer = new List<Vector2>();
             _normalBuffer = new List<Vector3>();
+        }
 
-            for (var x = 0; x < gridSize.x; x++)
+        public void InitDecor(Vector2Int size, int decoWidth)
+        {
+            var row = Enumerable.Repeat(1, decoWidth).ToArray();
+            var grid = Enumerable.Repeat(row, size.y).ToArray();
+            var decorationMesh = new Mesh();
+            UpdateMesh(grid, 0, decorationMesh);
+            decorationLeft.mesh = decorationMesh;
+            decorationLeft.transform.localPosition = new Vector3(-decoWidth, 0, 0);
+            decorationRight.mesh = decorationMesh;
+            decorationRight.transform.localPosition = new Vector3(size.x, 0, 0);
+        }
+
+        public void UpdateMesh(int[][] grid, int offset, Mesh updateMesh = null)
+        {
+            ClearBuffers();
+            for (var i = 0; i < grid.Length; i++)
             {
-                for (var y = 0; y < gridSize.y; y++)
+                var row = grid[(i + offset) % grid.Length];
+                for (var j = 0; j < row.Length; j++)
                 {
-                    var startFaceIndex = _verticeBuffer.Count;
-                    var frontFaceMesh = frontFaceTemplate;
+                    var pos = new Vector3(j, -i, 0);
+                    var element = row[j];
 
-                    _verticeBuffer.AddRange(frontFaceMesh.vertices.Select(v => v + new Vector3(x, y, 0)));
-                    _uvBuffer.AddRange(frontFaceMesh.uv);
-                    _normalBuffer.AddRange(frontFaceMesh.normals);
+                    if (element == 0) // hole
+                    {
+                        AddMeshTemplate(backFaceTemplate, pos, _uvOffsets[element]);
 
-                    _triangleBuffer.AddRange(frontFaceMesh.triangles.Select(t => t + startFaceIndex));
+                        var left = j <= 0 || row[j - 1] > 0;
+                        if (left)
+                        {
+                            AddMeshTemplate(rightFaceTemplate, pos + Vector3.left, _uvOffsets[element]);
+                        }
+
+                        var right = j >= row.Length - 1 || row[j + 1] > 0;
+                        if (right)
+                        {
+                            AddMeshTemplate(leftFaceTemplate, pos + Vector3.right, _uvOffsets[element]);
+                        }
+
+                        var previousRow = grid[(i + offset + grid.Length - 1) % grid.Length];
+
+                        var top = previousRow[j] > 0;
+
+                        if (top)
+                        {
+                            AddMeshTemplate(bottomFaceTemplate, pos + Vector3.up, _uvOffsets[element]);
+                        }
+
+                        var nextRow = grid[(i + offset + 1) % grid.Length];
+                        var bottom = nextRow[j] > 0;
+
+                        if (bottom)
+                        {
+                            AddMeshTemplate(topFaceTemplate, pos + Vector3.down, _uvOffsets[element]);
+                        }
+                    }
+                    else // wall
+                    {
+                        AddMeshTemplate(frontFaceTemplate, pos, _uvOffsets[element]);
+                    }
                 }
             }
 
-            mesh.vertices = _verticeBuffer.ToArray();
-            mesh.triangles = _triangleBuffer.ToArray();
+            UpdateMesh(updateMesh == null ? _mesh : updateMesh);
+        }
+
+        private void UpdateMesh(Mesh mesh)
+        {
+            mesh.vertices = _vertexBuffer.ToArray();
             mesh.uv = _uvBuffer.ToArray();
             mesh.normals = _normalBuffer.ToArray();
+            mesh.triangles = _triangleBuffer.ToArray();
+            mesh.UploadMeshData(false);
+            Debug.Log($"{mesh.vertexCount}{mesh.uv.Length}{mesh.normals.Length}{mesh.triangles.Length}");
+        }
+
+        private void ClearBuffers()
+        {
+            _vertexBuffer.Clear();
+            _triangleBuffer.Clear();
+            _uvBuffer.Clear();
+            _normalBuffer.Clear();
+            _mesh.Clear();
+        }
+
+        private void AddMeshTemplate(Mesh meshTemplate, Vector3 positionOffset)
+        {
+            AddMeshTemplate(meshTemplate, positionOffset, Vector2.zero);
+        }
+
+        private void AddMeshTemplate(Mesh meshTemplate, Vector3 positionOffset, Vector2 uvOffset)
+        {
+            var startFaceIndex = _vertexBuffer.Count;
+
+            _vertexBuffer.AddRange(positionOffset != Vector3.zero
+                ? meshTemplate.vertices.Select(v => v + positionOffset)
+                : meshTemplate.vertices);
+
+            _uvBuffer.AddRange(uvOffset != Vector2.zero
+                ? meshTemplate.uv.Select(uv => uv + uvOffset)
+                : meshTemplate.uv);
+
+            _normalBuffer.AddRange(meshTemplate.normals);
+
+            _triangleBuffer.AddRange(meshTemplate.triangles.Select(t => t + startFaceIndex));
         }
     }
 }
